1ca41c3d4dcd3546a61efb5c687a74d0b252a7fa
phd91105 <duy.ph@briswell-vn.com>
2021-07-18 00:08:44 +0700

    update

--- src/migrations/1626541616100-init-model.ts
+++ src/migrations/1626541616100-init-model.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class uniqueUsername1626529840675 implements MigrationInterface {
-    name = 'uniqueUsername1626529840675'
+export class initModel1626541616100 implements MigrationInterface {
+    name = 'initModel1626541616100'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("ALTER TABLE `category` CHANGE `deletedAt` `deletedAt` datetime(6) NULL");
@@ -11,21 +11,25 @@ export class uniqueUsername1626529840675 implements MigrationInterface {
         await queryRunner.query("ALTER TABLE `food` CHANGE `details` `details` varchar(255) NULL");
         await queryRunner.query("ALTER TABLE `food` CHANGE `image` `image` varchar(255) NULL");
         await queryRunner.query("ALTER TABLE `food` CHANGE `catId` `catId` int NULL");
-        await queryRunner.query("ALTER TABLE `order` CHANGE `deletedAt` `deletedAt` datetime(6) NULL");
+        await queryRunner.query("ALTER TABLE `order_details` DROP FOREIGN KEY `FK_147bc15de4304f89a93c7eee969`");
         await queryRunner.query("ALTER TABLE `order_details` CHANGE `deletedAt` `deletedAt` datetime(6) NULL");
+        await queryRunner.query("ALTER TABLE `order_details` CHANGE `orderId` `orderId` bigint NULL");
+        await queryRunner.query("ALTER TABLE `order` CHANGE `deletedAt` `deletedAt` datetime(6) NULL");
         await queryRunner.query("ALTER TABLE `section` CHANGE `deletedAt` `deletedAt` datetime(6) NULL");
         await queryRunner.query("ALTER TABLE `section` CHANGE `remarks` `remarks` text NULL");
         await queryRunner.query("ALTER TABLE `user` CHANGE `deletedAt` `deletedAt` datetime(6) NULL");
-        await queryRunner.query("ALTER TABLE `user` ADD UNIQUE INDEX `IDX_78a916df40e02a9deb1c4b75ed` (`username`)");
+        await queryRunner.query("ALTER TABLE `order_details` ADD CONSTRAINT `FK_147bc15de4304f89a93c7eee969` FOREIGN KEY (`orderId`) REFERENCES `order`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `user` DROP INDEX `IDX_78a916df40e02a9deb1c4b75ed`");
+        await queryRunner.query("ALTER TABLE `order_details` DROP FOREIGN KEY `FK_147bc15de4304f89a93c7eee969`");
         await queryRunner.query("ALTER TABLE `user` CHANGE `deletedAt` `deletedAt` datetime(6) NULL DEFAULT 'NULL'");
         await queryRunner.query("ALTER TABLE `section` CHANGE `remarks` `remarks` text NULL DEFAULT 'NULL'");
         await queryRunner.query("ALTER TABLE `section` CHANGE `deletedAt` `deletedAt` datetime(6) NULL DEFAULT 'NULL'");
-        await queryRunner.query("ALTER TABLE `order_details` CHANGE `deletedAt` `deletedAt` datetime(6) NULL DEFAULT 'NULL'");
         await queryRunner.query("ALTER TABLE `order` CHANGE `deletedAt` `deletedAt` datetime(6) NULL DEFAULT 'NULL'");
+        await queryRunner.query("ALTER TABLE `order_details` CHANGE `orderId` `orderId` bigint NULL DEFAULT 'NULL'");
+        await queryRunner.query("ALTER TABLE `order_details` CHANGE `deletedAt` `deletedAt` datetime(6) NULL DEFAULT 'NULL'");
+        await queryRunner.query("ALTER TABLE `order_details` ADD CONSTRAINT `FK_147bc15de4304f89a93c7eee969` FOREIGN KEY (`orderId`) REFERENCES `order`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
         await queryRunner.query("ALTER TABLE `food` CHANGE `catId` `catId` int NULL DEFAULT 'NULL'");
         await queryRunner.query("ALTER TABLE `food` CHANGE `image` `image` varchar(255) NULL DEFAULT 'NULL'");
         await queryRunner.query("ALTER TABLE `food` CHANGE `details` `details` varchar(255) NULL DEFAULT 'NULL'");
