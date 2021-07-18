b855ffa94dbe66ea44364f87e44ab312648cee11
phd91105 <duy.ph@briswell-vn.com>
2021-07-18 17:15:31 +0700

    update

--- src/middlewares/api.middleware.ts
+++ src/middlewares/api.middleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { extractJWT } from '../utils/jwt';
 import { errorLog } from '../utils/logger';
 
 export function authenticate(
@@ -10,6 +11,14 @@ export function authenticate(
     errorLog(req, 'unauthorized');
     res.status(400).json({ message: 'Unauthorized' });
     return;
+  } else {
+    try {
+      extractJWT(req.headers!.authorization);
+    } catch {
+      errorLog(req, 'unauthorized');
+      res.status(400).json({ message: 'Unauthorized' });
+      return;
+    }
   }
   next();
 }
