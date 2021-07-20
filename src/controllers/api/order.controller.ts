74a623f68ee2b954268319fab18693fc69bb9ff6
phd91105 <duy.ph@briswell-vn.com>
2021-07-20 15:52:43 +0700

    update

--- src/controllers/api/order.controller.ts
+++ src/controllers/api/order.controller.ts
@@ -3,6 +3,7 @@ import { getRepository } from 'typeorm';
 import { connectdb } from '../../config/db.config';
 import { OrderStatus } from '../../constants';
 import { Cart } from '../../models/cart.model';
+import { Food } from '../../models/food.model';
 import { Order } from '../../models/order.model';
 import { OrderDetails } from '../../models/orderDetails.model';
 import { extractJWT } from '../../utils/jwt';
@@ -16,6 +17,27 @@ async function getOrderList(req: Request, res: Response): Promise<Response> {
     .where({ userId: extractJWT(<string>req.headers!.authorization).uid })
     .getMany();
   return res.json(orders).status(200);
+}
+
+async function getOrderDetails(req: Request, res: Response): Promise<void> {
+  const orderRepository = getRepository(Order);
+  const orderItems = await orderRepository
+    .createQueryBuilder('order')
+    .leftJoinAndMapMany(
+      'order.order_details',
+      OrderDetails,
+      'order_details',
+      'order.id = order_details.orderId',
+    )
+    .leftJoinAndMapOne(
+      'order_details.food',
+      Food,
+      'food',
+      'order_details.foodId = food.id',
+    )
+    .where({ id: +req.params.id })
+    .getOne();
+  res.json(orderItems).status(200);
 }
 
 async function createOrder(req: Request, res: Response): Promise<Response> {
@@ -44,4 +66,4 @@ async function createOrder(req: Request, res: Response): Promise<Response> {
   return res.json(createOrder).status(200);
 }
 
-export { getOrderList, createOrder };
+export { getOrderList, createOrder, getOrderDetails };
--- src/routes/api/order.route.ts
+++ src/routes/api/order.route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import {
   createOrder,
+  getOrderDetails,
   getOrderList,
 } from '../../controllers/api/order.controller';
 import { authenticate } from '../../middlewares/api.middleware';
@@ -8,6 +9,7 @@ import { authenticate } from '../../middlewares/api.middleware';
 const orderApiRouter: Router = Router();
 
 orderApiRouter.get('/orders', authenticate, getOrderList);
+orderApiRouter.get('/order/:id', authenticate, getOrderDetails);
 orderApiRouter.post('/order', authenticate, createOrder);
 
 export { orderApiRouter };
